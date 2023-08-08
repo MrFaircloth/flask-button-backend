e7840bbfaf3446e6c9ec215cdb9178cd1e1eb1cf
Mitch Faircloth <faircloth@faircloth.local>
2023-08-07 19:33:38 -0700

    updated utils and added test

--- button/util.py
+++ button/util.py
@@ -23,19 +23,14 @@ def get_future_timestamp(
 
 
 def get_time_difference(start_time: datetime, end_time: datetime) -> str:
-    # Calculate the time difference between the two datetimes
     time_difference = end_time - start_time
-
-    # Convert the time difference to total seconds
     total_seconds = int(time_difference.total_seconds())
-
-    # Calculate the components (days, hours, minutes, seconds)
+    
     days = time_difference.days
-    hours = total_seconds // 3600
+    hours = total_seconds // 3600 % 24
     minutes = (total_seconds % 3600) // 60
     seconds = total_seconds % 60
-
-    # Build the human-readable string
+    
     result = ""
     if days > 0:
         result += f"{days} days"
@@ -43,7 +38,7 @@ def get_time_difference(start_time: datetime, end_time: datetime) -> str:
         result += f", {hours} hours"
     if minutes > 0:
         result += f", {minutes} minutes"
-    if seconds > 0 and days == 0 and hours == 0 and minutes == 0:
+    if seconds > 0:
         result += f", {seconds} seconds"
-
+    
     return result.strip(', ')
